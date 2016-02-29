0d1675d4da6c24f0afce3b62a7cf8937ffcb81ba
thalesalexcin@gmail.com <thalesalexcin@gmail.com>
2016-02-29 03:44:10 +0100

    final push

---
+++ Code/Assets/Scripts/MovingCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCamera : MonoBehaviour {
+
+    public Vector3 Velocity;
+    public float ShowAfter;
+    private float _Timer;
+    private MeshRenderer _Renderer;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _Renderer = GetComponent<MeshRenderer>();
+        if (ShowAfter > 0 && _Renderer != null)
+            _Renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Velocity * Time.deltaTime);
+
+        _Timer += Time.deltaTime;
+        if (_Timer >= ShowAfter && _Renderer != null)
+            _Renderer.enabled = true;
+	}
+}
